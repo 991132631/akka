36b28918be90a4a5371a6a26fe37387e6a7b4520
zhx <991132631@qq.com>
2021-07-18 13:13:23 +0800

    study scala akka project init

---
+++ src/main/scala/test/HelloActor.scala
@@ -0,0 +1,43 @@
+package test
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import test.HelloActor.{helloActor, helloActorRef}
+
+
+class HelloActor extends  Actor{
+  override def receive: Receive = {
+
+    case "你好帅" =>println("竟说实话")
+    case "你好丑" => {
+      println("滚，不跟你说了，我关闭系统了")
+
+      /**
+       * 发消息，关闭actor，传入的都是actor的引用地址，ActorRef
+       * 关闭actor，self==当前actor，即helloActorRef
+       */
+      //helloActor.stop(helloActorRef)
+      //helloActor.stop(self)
+      //context.stop(helloActorRef)
+      context.stop(self)
+      //关闭jvm里akka的ActorSystem
+      context.system.terminate()
+    }
+
+  }
+}
+
+/**
+ *extends App,App Trait可以帮你实现类的懒加载？
+ *可以直接实现main方法，不用写main直接写内容
+ */
+object HelloActor extends App {
+  private val helloActor: ActorSystem = ActorSystem("HelloActor")
+  private val helloActorRef: ActorRef = helloActor.actorOf(Props[HelloActor], "helloActor")
+
+  helloActorRef ! "你好帅"
+  helloActorRef ! "你好丑"
+
+
+
+
+}
