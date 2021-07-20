41860a9b59818ad63d16c5af5c044c6d576cc6ef
zhx <991132631@qq.com>
2021-07-20 10:54:38 +0800

    study scala akka project init

--- src/main/scala/spark/SparkMaster.scala
+++ src/main/scala/spark/SparkMaster.scala
@@ -7,10 +7,12 @@ import scala.concurrent.duration._
 
 class SparkMaster extends Actor {
 
+  // 存储worker发来的注册的worker节点信息的
   val liveWorkerNodeMap = collection.mutable.HashMap[String, WorkerInfo]()
 
   override def receive: Receive = {
 
+    // 收到worker注册过来的信息
     case RegisterWorkerInfo(workerId, core, ram) => {
 
       if (!liveWorkerNodeMap.contains(workerId)) {
@@ -21,15 +23,18 @@ class SparkMaster extends Actor {
         //liveWorkerNodeMap += ((workerId, workerInfo))
         liveWorkerNodeMap.put(workerId, workerInfo)
       }
+      // master存储完worker注册的数据之后，要告诉worker说你已经注册成功
       sender() ! RegisteredWorkerInfo
       println("master完成worker节点注册")
     }
 
     case HeartBeat(workerId) => {
+      // master收到worker的心跳消息之后，更新woker的上一次心跳时间
 
       //语法糖map.get(key)==map(key)   ->  val workerInfo = liveWorkerNodeMap.get(workerId)
       val workerInfo = liveWorkerNodeMap(workerId)
       val currentTimeMillis = System.currentTimeMillis()
+      // 更改心跳时间
       workerInfo.lastHeartBeatTime = currentTimeMillis
       liveWorkerNodeMap.put(workerId, workerInfo)
       println("master更新存活worker节点心跳检测时间")
@@ -37,12 +42,15 @@ class SparkMaster extends Actor {
     }
 
     case CheckTimeOutWorker => {
+      // 使用调度器时候必须导入dispatcher
       import context.dispatcher
       context.system.scheduler.schedule(0 millis, 6000 millis, self, RemoveTimeOurWorker)
       println("定时对记录的worker的心跳时间检测是否超时")
     }
 
     case RemoveTimeOurWorker => {
+      // 将hashMap中的所有的value都拿出来，查看当前时间和上一次心跳时间的差 3000
+
       val workInfos = liveWorkerNodeMap.values
       val currentTime = System.currentTimeMillis()
 
@@ -50,6 +58,7 @@ class SparkMaster extends Actor {
        * 注意一下这个
        * x => liveWorkerNodeMap.remove(x.id) 是因为WorkerInfo类里val了id才能用，这个原因是什么
        */
+      //  过滤超时的worker
       workInfos.filter(x => currentTime - x.lastHeartBeatTime > 3000).foreach(x => liveWorkerNodeMap.remove(x.id))
       println(s"-------还剩下${liveWorkerNodeMap.size}个worker节点存活 -------")
     }
@@ -84,7 +93,8 @@ object SparkMaster {
 
     val sparkMasterSystem = ActorSystem("sparkMasterSystem", config)
     val sparkMasterRef = sparkMasterSystem.actorOf(Props[SparkMaster], masterName)
-
+    // 自己给自己发送一个消息，去启动一个调度器，定期的检测HashMap中超时的worker
+    //最开始没有节点也不影响，有了的话就开始执行检查
     sparkMasterRef ! CheckTimeOutWorker
 
 
--- src/main/scala/spark/SparkWorker.scala
+++ src/main/scala/spark/SparkWorker.scala
@@ -6,9 +6,12 @@ import java.util.UUID
 import scala.concurrent.duration._
 
 
+//SparkWorker先向master发消息，所以需要拿到master的信息，(sparkMasterURL: String)
 class SparkWorker(sparkMasterURL: String) extends Actor {
 
+  //获取server端的ActorRef我们用的是ActorSelection
   var masterProxy: ActorSelection = _
+  //worker节点id
   val workerId = UUID.randomUUID().toString
 
   override def preStart(): Unit = {
@@ -17,15 +20,19 @@ class SparkWorker(sparkMasterURL: String) extends Actor {
 
   override def receive: Receive = {
     case "start" =>
+      // 向master注册自己的信息，id, core, ram
       masterProxy ! RegisterWorkerInfo(workerId, 4, 64 * 1024)
       println("worker节点向master节点注册")
 
     case RegisteredWorkerInfo => {
+      // 收到master发送给worker已经的注册成功消息
+      // worker 启动一个定时器，定时向master 发送心跳
       import context.dispatcher
       context.system.scheduler.schedule(0 millis, 1500 millis, self, SendHeartBeat)
       println("worker节点定时提醒自己发送心跳")
     }
     case SendHeartBeat => {
+      // 开始向master发送心跳了
       println(s"-------worker节点 $workerId 发送给Master心跳检测-----")
       masterProxy ! HeartBeat(workerId)
     }
@@ -62,7 +69,7 @@ object SparkWorker {
 
     val SparkWorkerSystem = ActorSystem("SparkWorkerSystem", config)
     val SparkWorkerRef = SparkWorkerSystem.actorOf(Props(new SparkWorker02(sparkMasterURL)), SparkWorkerName)
-
+    // 给自己发送一个以启动的消息，标识自己已经就绪了，自己会接收到，然后作为标志执行接下来的逻辑--给master发送注册消息
     SparkWorkerRef ! "start"
 
 
